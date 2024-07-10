9fb5bfc555acb90473c2f2f0690b73d21f4667b3
Vinit Shahdeo <vinitshahdeo@gmail.com>
2024-07-11 02:59:53 +0530

    Added google verification

--- src/config/analytics.ts
+++ src/config/analytics.ts
@@ -1,1 +1,2 @@
 export const measurementId = "G-0NWZDRMJMF"; // @note: Please update this value with your own Measurement ID.
+export const googleSiteVerification = "4tDbwC4MwIRDfltBaqtepRknsSlvnvWizTPIQCoZWWg"; // @note: Please update this value with your own Google Site Verification ID.

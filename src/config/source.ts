f57dcf6ed4783e9a8fdcde015f0b0b2bf1770344
Vinit Shahdeo <vinitshahdeo@gmail.com>
2024-07-15 13:42:36 +0530

    Fixed GitHub Repo link

--- src/config/source.ts
+++ src/config/source.ts
@@ -3,13 +3,13 @@ import type { SourceLink } from "../types/config";
 export const sourceLinks: SourceLink[] = [
   {
     title: "View GitHub Repository",
-    url: "https://github.com/vinitshahdeo",
+    url: "https://github.com/vinitshahdeo/portfolio",
     icon: "mdi:github",
     external: true,
   },
   {
     title: "Download Astro Template",
-    url: "https://github.com/vinitshahdeo", // @todo: replace with Astro link
+    url: "https://github.com/vinitshahdeo/portfolio", // @todo: replace with Astro link
     icon: "mdi:code-block-tags",
     external: true,
   },
